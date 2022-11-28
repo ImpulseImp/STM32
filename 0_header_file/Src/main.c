bdca8a2d38fb8bbf0baa7148d34e656acf15d13a
Impulse <ImpulsEmbedded@gmail.com>
2022-11-28 19:09:09 +0300

    init commit imp

---
+++ 0_header_file/Src/main.c
@@ -0,0 +1,20 @@
+#include "stm32f411xe.h"
+
+#define GPIOAEN				(1U<<0)
+
+
+
+int main()
+{
+    /*Enable clock access to GPIO A*/
+	RCC->AHB1ENR |= GPIOAEN;
+
+	/*Set PA5 to output pin*/
+	GPIOA->MODER |= (1U<<10);
+	GPIOA->MODER &= ~(1U<<11);
+
+	/*Set PA5 high*/
+	GPIOA->ODR |=(1U<<5);
+
+
+}
